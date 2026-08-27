6f36ed197a744645d2df4971f988094c3860490e
ripboo <dokan10010@gmail.com>
2026-08-27 19:07:19 +0200

    Add g_bDumpThreadRegisters for linker error fix
    Added g_bDumpThreadRegisters variable to manage thread register dumping.

--- main.cpp
+++ main.cpp
@@ -55,7 +55,8 @@ static const char* TARGET_GAME_LIB = "libGTASA.so";
 
 pid_t g_MainThreadID = 0;
 bool g_bShowUpdatedToast, g_bShowUpdateFailedToast, g_bEnableFileDownloads;
-bool g_bCrashAML, g_bNoMods, g_bSimplerCrashLog = false, g_bNoSPInLog, g_bNoModsInLog, g_bMLSOnlyManualSaves, g_bDumpAllThreads, g_bEHUnwind, g_bMoreRegsInfo, g_bUnixBacktrace = false;
+// تم إرجاع المتغير g_bDumpThreadRegisters هنا لحل خطأ الربط (Linker Error)
+bool g_bCrashAML, g_bNoMods, g_bSimplerCrashLog = false, g_bNoSPInLog, g_bNoModsInLog, g_bMLSOnlyManualSaves, g_bDumpAllThreads, g_bEHUnwind, g_bMoreRegsInfo, g_bDumpThreadRegisters = false, g_bUnixBacktrace = false;
 int g_nEnableNews, g_nDownloadTimeout;
 int g_nAndroidSDKVersion = 0, g_nFailedToLoad = 0, g_nLatestDownloadErrorCode = 0;
 ConfigEntry* g_pLastNewsId;
@@ -727,6 +728,7 @@ void StartAMLRightNow(const char* libName1 = NULL, const char* libName2 = NULL)
     g_bDumpAllThreads = cfg->GetBool("CrashLogFromAllThreads", true, "DevTools");
     g_bEHUnwind = cfg->GetBool("EHUnwindCrashLog", false, "DevTools");
     g_bMoreRegsInfo = cfg->GetBool("MoreRegistersInfo", true, "DevTools");
+    g_bDumpThreadRegisters = cfg->GetBool("DumpThreadRegisters", false, "DevTools");
 
     cfg->Save();
 
